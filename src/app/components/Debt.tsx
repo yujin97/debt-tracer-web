1669cf9e3f6d586cd244dc308d286e4a7a2c0611
Eugene <eugene97@connect.hku.hk>
2024-05-19 19:11:32 +0900

    feat: create skeleton of Debt List

---
+++ src/app/components/Debt.tsx
@@ -0,0 +1,29 @@
+interface Props {
+  type: "borrowed" | "lent";
+  date: string;
+  amount: number;
+  currency: string;
+  subject: string;
+  status: "PAID" | "UNPAID";
+}
+export function Debt({ type, date, amount, currency, subject, status }: Props) {
+  const typeLabel = type === "borrowed" ? "Borrowed" : "Lent";
+  const statusLabel = status === "PAID" ? "PAID" : "UNPAID";
+  const subjectLabel = type === "borrowed" ? "Creditor" : "Debtor";
+
+  return (
+    <div className="flex flex-col border p-1">
+      <div className="flex justify-between">
+        <div className="border border-sky-50 p-1">{typeLabel}</div>
+        <div>{date}</div>
+      </div>
+      <div className="p-2">{`${amount} ${currency}`}</div>
+      <div className="flex justify-between">
+        <div>
+          {subjectLabel}: {subject}
+        </div>
+        <div className="border p-1">{statusLabel}</div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import { Debt } from "./components/Debt";
+
 export default function Home() {
   return (
-    <main className="min-h-screen p-24">
-      <h1>Debt Tracer</h1>
+    <main className="flex min-h-screen flex-col p-12">
+      <div>
+        <h1>Debt Tracer</h1>
+      </div>
+      <div className="flex flex-col p-4">
+        <Debt
+          type="borrowed"
+          date="2024-05-19"
+          amount={420.69}
+          currency="USD"
+          subject="Oscar"
+          status="PAID"
+        />
+        <Debt
+          type="borrowed"
+          date="2024-05-19"
+          amount={420.69}
+          currency="USD"
+          subject="Oscar"
+          status="PAID"
+        />
+      </div>
     </main>
   );
 }
