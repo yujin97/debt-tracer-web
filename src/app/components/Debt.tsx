17c215706f3a806b45cf4bf2fdd5d82083fa82b0
Eugene <eugene97@connect.hku.hk>
2024-06-22 23:21:12 +0900

    style: adjust debt card styles

--- src/app/components/Debt.tsx
+++ src/app/components/Debt.tsx
@@ -13,7 +13,7 @@ export function Debt({ type, date, amount, currency, subject, status }: Props) {
   const subjectLabel = type === "borrowed" ? "Creditor" : "Debtor";
 
   return (
-    <div className="flex flex-col rounded-md border p-2">
+    <div className="flex flex-col rounded-md border-zinc-300 bg-gray-950 p-2 shadow-inner shadow-white">
       <div className="flex justify-between">
         <div className="rounded-md border border-sky-50 p-1">{typeLabel}</div>
         <div>{date}</div>
